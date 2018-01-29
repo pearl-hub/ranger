17a9f7b49ae8b9f5c0dceb0f15bb85d6f33d0f27
Filippo Squillace <squillace.filippo@gmail.com>
2018-01-29 12:37:30 +1100

    Update to ranger 1.9.0

--- pearl-metadata/install.sh
+++ pearl-metadata/install.sh
@@ -1,5 +1,8 @@
 function post_install(){
-    link_to_path "${PEARL_PKGDIR}/module/scripts/ranger"
+    # TODO use newer `link_to_path` function for this
+    local executable_path="${PEARL_PKGDIR}/module/ranger.py"
+    local executable_name="ranger"
+    link_to "${executable_path}" "${PEARL_HOME}/bin/${executable_name}"
 }
 
 function post_update(){
@@ -7,5 +10,8 @@ function post_update(){
 }
 
 function pre_remove(){
-    unlink_from_path "${PEARL_PKGDIR}/module/scripts/ranger"
+    # TODO use newer `unlink_from_path` function for this
+    local executable_path="${PEARL_PKGDIR}/module/ranger.py"
+    local executable_name="ranger"
+    unlink_from "${executable_path}" "${PEARL_HOME}/bin/${executable_name}"
 }
