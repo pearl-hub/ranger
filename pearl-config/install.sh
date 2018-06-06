a956a71f478874142b6da5049e20ac1c605c4ce3
Filippo Squillace <squillace.filippo@gmail.com>
2018-06-06 21:47:50 +1000

    Update rifle.conf and get dynamic updates from ranger repo

--- pearl-config/install.sh
+++ pearl-config/install.sh
@@ -1,16 +1,30 @@
-function post_install(){
-    link_to_path "${PEARL_PKGDIR}/module/ranger.py" "ranger"
+function _init(){
+    link_to_path "${PEARL_PKGVARDIR}/ranger/ranger.py" "ranger"
 
     mkdir -p ${HOME}/.config/ranger/
     cp -f ${PEARL_PKGDIR}/rifle.conf ${HOME}/.config/ranger/
 }
 
+
+function post_install(){
+    git clone https://github.com/ranger/ranger.git ${PEARL_PKGVARDIR}/ranger
+
+    _init
+}
+
 function post_update(){
-    post_install
+    cd ${PEARL_PKGVARDIR}/ranger
+    git fetch --quiet --all
+    git reset --quiet --hard origin/master
+    git submodule update --quiet --init --recursive
+    git checkout --quiet master
+
+    _init
 }
 
 function pre_remove(){
-    unlink_from_path "${PEARL_PKGDIR}/module/ranger.py" "ranger"
+    unlink_from_path "${PEARL_PKGVARDIR}/ranger/ranger.py" "ranger"
 
     rm -f ${HOME}/.config/ranger/rifle.conf
+    rm -rf ${PEARL_PKGVARDIR}/ranger
 }
