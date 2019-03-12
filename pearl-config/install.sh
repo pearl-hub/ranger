d42eed42649e30e0124f090643f5df2b4f7eb9e6
Filippo Squillace <squillace.filippo@gmail.com>
2019-03-12 20:08:05 +1100

    Change shebang for ranger script

--- pearl-config/install.sh
+++ pearl-config/install.sh
@@ -2,7 +2,10 @@ function post_install(){
     info "Installing or updating the ranger git repository..."
     install_or_update_git_repo https://github.com/ranger/ranger.git "${PEARL_PKGVARDIR}/ranger" master
 
-    link_to_path "${PEARL_PKGVARDIR}/ranger/ranger.py" "ranger"
+    # The following cannot be used because the shebang is hardcoded in ranger.py:
+    #link_to_path "${PEARL_PKGVARDIR}/ranger/ranger.py" "ranger"
+    echo "/usr/bin/env python -O ${PEARL_PKGVARDIR}/ranger/ranger.py" >> ${PEARL_HOME}/bin/ranger
+    chmod +x ${PEARL_HOME}/bin/ranger
 
     mkdir -p ${HOME}/.config/ranger/
     cp -f ${PEARL_PKGDIR}/rifle.conf ${HOME}/.config/ranger/
@@ -13,7 +16,7 @@ function post_update(){
 }
 
 function pre_remove(){
-    unlink_from_path "${PEARL_PKGVARDIR}/ranger/ranger.py" "ranger"
+    rm -f ${PEARL_HOME}/bin/ranger
 
     rm -f ${HOME}/.config/ranger/rifle.conf
     rm -rf ${PEARL_PKGVARDIR}/ranger
